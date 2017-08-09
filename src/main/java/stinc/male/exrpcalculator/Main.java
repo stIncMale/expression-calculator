1bd8ccf72a2a098a72ddba76e9d070cedde50be7
Valentin Kovalenko <valentin.male.kovalenko@gmail.com>
2017-08-09 08:48:46 +0300

    Initial commit

---
+++ src/main/java/stinc/male/exrpcalculator/Main.java
@@ -0,0 +1,11 @@
+package stinc.male.exrpcalculator;
+
+public final class Main {
+
+    public static final void main(final String[] args) {
+    }
+
+    private Main() {
+        throw new UnsupportedOperationException("This class is not dedigned to be instantiated");
+    }
+}
---
+++ src/main/java/stinc/male/exrpcalculator/package-info.java
@@ -0,0 +1,1 @@
+package stinc.male.exrpcalculator;
